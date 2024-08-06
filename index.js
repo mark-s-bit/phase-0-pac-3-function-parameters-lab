01c43cb5f3cede96c15810dc7ea71ca36bc8ab4a
mark-s-bit <markchege03@gmail.com>
2024-08-07 00:27:36 +0300

    complete

---
+++ index.js
@@ -0,0 +1,15 @@
+let name = "AK1"
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+
+let language = "Ember.js."
+
+function introductionWithLanguage(name, language){
+    return  `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
